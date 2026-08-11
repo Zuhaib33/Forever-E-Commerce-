68dc4e2fd7558732ca10280086b29bc950dd7b27
Zuhaib33 <m.zuhaibofficial@gmail.com>
2026-08-10 23:37:57 -0700

    move to admin section

---
+++ admin/src/App.jsx
@@ -0,0 +1,33 @@
+ import React from "react"
+ import Navbar from './components/Navbar'
+ import SideBar from './components/SideBar'
+ import { Route,Routes } from "react-router-dom"
+ import Add from './pages/Add'
+ import List from './pages/List'
+ import Orders from './pages/Orders'
+ 
+
+function App() {
+
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+     <Navbar/>
+     <hr />
+     <div className="flex w-full">
+      <SideBar/>
+      <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default App
