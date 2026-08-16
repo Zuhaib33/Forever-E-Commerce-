9f755a196afd92c0d4341bcda7ab56b764a43dfa
Zuhaib33 <m.zuhaibofficial@gmail.com>
2026-08-15 21:35:07 -0700

    create add and login page

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-4 px[4%] justify-between'>
       <img src={assets.logo} alt="" className='w-[max(10%,80px)]' />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:text-sm'>LogOut</button>
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:text-sm' onClick={()=>setToken("")}>LogOut</button>
     </div>
   )
 }
--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -7,7 +7,7 @@ const adminAuth = async (req,res,next)=>{
         const {token} = req.headers
 
     if(!token){
-       return res.json({success:false,message:"Not Authorized Login Again "})
+       return res.json({success:false,message:"Not Authorized Login Again  "})
     }
 
     const token_decode=jwt.verify(token,process.env.JWT_SECRET)
